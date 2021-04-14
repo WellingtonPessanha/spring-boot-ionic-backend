7d92dd40d7e73ca8e569b0c592944899cc6a2c7f
Wellington Pessanha <ricardo.wellington@hotmail.com>
2021-04-14 17:32:04 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -39,9 +39,13 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
+	
 	public Cliente() {
-	}
-
+	}	
+	
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
 		super();
 		this.id = id;
@@ -106,6 +110,15 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}		
+	
 
 	@Override
 	public int hashCode() {
@@ -131,8 +144,6 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 		
-	
-	
 }
